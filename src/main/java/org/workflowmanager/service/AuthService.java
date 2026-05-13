6d327a6a5385e6a3f6b616a3933bbe8a3be74bb4
vNullptr <ghstdu@gmail.com>
2026-05-13 23:06:06 +0100

    feat: implement AuthService with BCrypt password verification

--- src/main/java/org/workflowmanager/service/AuthService.java
+++ src/main/java/org/workflowmanager/service/AuthService.java
@@ -1,1 +1,30 @@
 package org.workflowmanager.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.workflowmanager.dao.UtilisateurDAO;
+import org.workflowmanager.model.Utilisateur;
+
+import java.sql.SQLException;
+
+public class AuthService {
+
+    private final UtilisateurDAO utilisateurDAO = new UtilisateurDAO();
+
+    public Utilisateur seConnecter(String email, String motDePasse) throws SQLException {
+        Utilisateur utilisateur = utilisateurDAO.findByEmail(email);
+
+        if (utilisateur == null) {
+            return null;
+        }
+
+        if (!BCrypt.checkpw(motDePasse, utilisateur.getMotDePasse())) {
+            return null;
+        }
+
+        return utilisateur;
+    }
+
+    public static String hacherMotDePasse(String motDePasse) {
+        return BCrypt.hashpw(motDePasse, BCrypt.gensalt());
+    }
+}
